5e19f62c967a7e3c8dc61f232c704b30135121d4
sunshineplan <sunshineplan@users.noreply.github.com>
2022-05-22 22:54:37 +0800

    Add listenURL function

--- common.go
+++ common.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"strings"
 
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/cdproto/fetch"
@@ -34,8 +35,48 @@ func listenFetch(ctx context.Context, actions ...chromedp.Action) error {
 	return chromedp.Run(ctx, append([]chromedp.Action{fetch.Enable()}, actions...)...)
 }
 
+func listenURL(ctx context.Context, url string) <-chan []byte {
+	c, done := make(chan []byte, 1), make(chan struct{}, 1)
+	var id network.RequestID
+	chromedp.ListenTarget(ctx, func(v any) {
+		switch ev := v.(type) {
+		case *network.EventRequestWillBeSent:
+			if strings.HasPrefix(ev.Request.URL, url) {
+				id = ev.RequestID
+			}
+		case *network.EventLoadingFinished:
+			if ev.RequestID == id {
+				done <- struct{}{}
+			}
+		}
+	})
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-done:
+			}
+
+			var b []byte
+			if err := chromedp.Run(
+				ctx,
+				chromedp.ActionFunc(func(ctx context.Context) (err error) {
+					b, err = network.GetResponseBody(id).Do(ctx)
+					return
+				}),
+			); err == nil {
+				c <- b
+			}
+		}
+	}()
+
+	return c
+}
+
 func listenPclog(ctx context.Context) <-chan struct{} {
-	done, c := make(chan struct{}, 1), make(chan struct{}, 1)
+	done, c := make(chan struct{}, 1), listenURL(ctx, pclogURL)
 	var n int
 	go func() {
 		for {
@@ -50,20 +91,6 @@ func listenPclog(ctx context.Context) <-chan struct{} {
 			}
 		}
 	}()
-
-	var id network.RequestID
-	chromedp.ListenTarget(ctx, func(v any) {
-		switch ev := v.(type) {
-		case *network.EventRequestWillBeSent:
-			if ev.Request.URL == pclogURL {
-				id = ev.RequestID
-			}
-		case *network.EventLoadingFinished:
-			if ev.RequestID == id {
-				c <- struct{}{}
-			}
-		}
-	})
 
 	return done
 }
