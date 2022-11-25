334331ce707118f32004864a23083a978e6ca848
sunshineplan <sunshineplan@users.noreply.github.com>
2022-11-25 15:29:37 +0800

    Update points.go

--- points.go
+++ points.go
@@ -62,7 +62,7 @@ func getPoints(ctx context.Context) (res pointsResult, err error) {
 }
 
 func (res pointsResult) CreateTask() task {
-	t := task{true, true, true, articleNumber, videoNumber}
+	t := task{false, true, true, articleNumber, videoNumber}
 	for _, i := range res.Data.TaskProgress {
 		switch i.Title {
 		case "每日答题":
